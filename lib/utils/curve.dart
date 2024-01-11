d3915de07a6cf1480034bc8adeba75c4059e5e98
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-11 16:57:22 +0100

    AddedM lib/utils/curve.dart

---
+++ lib/utils/curve.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class BezierCurve extends StatelessWidget {
+  const BezierCurve({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+      child: CustomPaint(
+        painter: Bezier(),
+      ),
+    );
+  }
+}
+
+class Bezier extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Path path = Path()..addPath(path, offset);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
