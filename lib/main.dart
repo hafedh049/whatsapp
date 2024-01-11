04c01533bbea675f096c314b232831a10809305c
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-11 16:34:27 +0100

    Modified  lib/main.dart M  lib/views/home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/views/home.dart';
 
 void main() {
   runApp(const Main());
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,1 +1,15 @@
-stf
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
