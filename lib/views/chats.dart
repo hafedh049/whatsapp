5ec5111ba1c73124f8cb37557372bab42cf86411
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-11 17:58:50 +0100

    ModifiedM lib/views/chats.dart

--- lib/views/chats.dart
+++ lib/views/chats.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/utils/shared.dart';
 
 class Chats extends StatefulWidget {
   const Chats({super.key});
@@ -8,9 +9,25 @@ class Chats extends StatefulWidget {
 }
 
 class _ChatsState extends State<Chats> {
+  final List<Widget> _choices = <Widget>[
+    GestureDetector(
+      child: Row(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(shape: BoxShape.circle),
+          ),
+        ],
+      ),
+    ),
+  ];
   final int _itemCount = 20;
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(itemBuilder:(BuildContext context,int index) =>  ,itemCount: _itemCount,separatorBuilder: (BuildContext context,int index) => ,);
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemBuilder: (BuildContext context, int index) => _choices[index % _choices.length],
+      itemCount: _itemCount,
+      separatorBuilder: (BuildContext context, int index) => Divider(thickness: .5, color: white.withOpacity(.6)),
+    );
   }
 }
