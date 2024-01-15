5b061c6abcacce56f3368baf557dba72320fe526
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-15 20:23:26 +0100

    Updates

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
-import 'package:whatsapp/utils/helpers/curve.dart';
 import 'package:whatsapp/utils/shared.dart';
 import 'package:whatsapp/views/chats.dart';
 
@@ -23,7 +22,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
 
   @override
   void initState() {
-    _tabsController = TabController(length: 5, vsync: this);
+    _tabsController = TabController(length: 4, vsync: this);
     super.initState();
   }
 
@@ -31,46 +30,55 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("WHATSAPP", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: white)),
-        centerTitle: true,
+        title: const Text("WhatsApp", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400, color: white, letterSpacing: 1.5)),
         actions: <Widget>[
           IconButton(
             onPressed: () {},
-            icon: const Icon(Bootstrap.grid_3x3_gap, size: 15, color: white),
+            icon: const Icon(Bootstrap.camera, size: 23, color: white),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Bootstrap.search, size: 20, color: white),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Bootstrap.grid_3x3_gap, size: 20, color: white),
           ),
         ],
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          const SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ClipPath(clipper: TanCurveClipper(), child: Container(color: white, width: double.infinity, height: 50)),
+          TabBar(
+            indicatorColor: green,
+            overlayColor: null,
+            dividerHeight: 0,
+            labelColor: green,
+            controller: _tabsController,
+            tabs: const <Tab>[
+              Tab(icon: Icon(Bootstrap.people, size: 20, color: white)),
+              Tab(text: "Chats"),
+              Tab(text: "Updates"),
+              Tab(text: "Calls"),
+            ],
           ),
-          const SizedBox(height: 30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TabBar(
-                  indicatorColor: green,
-                  overlayColor: null,
-                  dividerHeight: 0,
-                  labelColor: green,
-                  controller: _tabsController,
-                  tabs: const <Tab>[
-                    Tab(icon: Icon(Bootstrap.people, size: 20, color: white)),
-                    Tab(text: "Chats"),
-                    Tab(text: "Channels"),
-                    Tab(text: "Calls"),
-                    Tab(icon: Icon(Bootstrap.search, size: 18, color: white)),
+          StatefulBuilder(
+            key: null,
+            builder: (BuildContext context, void Function(void Function()) _) {
+              return GestureDetector(
+                onTap: () {},
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const Icon(Bootstrap.archive, size: 20, color: white),
+                    const SizedBox(width: 20),
+                    Text("I know that's what I said!", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: white.withOpacity(.8))),
                   ],
                 ),
-                Expanded(child: TabBarView(controller: _tabsController, children: <Widget>[for (int i = 0; i < 5; i++) const Chats()])),
-              ],
-            ),
+              );
+            },
           ),
+          Expanded(child: TabBarView(controller: _tabsController, children: <Widget>[for (int i = 0; i < 4; i++) const Chats()])),
         ],
       ),
     );
