c0cd766799cac0a6743c6456501ebe1d69993cda
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-24 22:55:26 +0100

    AddedM lib/views/channel/channel.dart

---
+++ lib/views/channel/channel.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Channel extends StatefulWidget {
+  const Channel({super.key});
+
+  @override
+  State<Channel> createState() => _ChannelState();
+}
+
+class _ChannelState extends State<Channel> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.separated(
+                itemBuilder: (BuildContext context, int index) => Container(),
+                separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 20),
+                itemCount: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
