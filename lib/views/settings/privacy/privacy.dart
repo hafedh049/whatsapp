9a780e736a988328e480194e1d4a31c0e15dbde6
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-26 15:12:16 +0100

    ModifiedM lib/views/settings/privacy/privacy.dart

--- lib/views/settings/privacy/privacy.dart
+++ lib/views/settings/privacy/privacy.dart
@@ -19,51 +19,56 @@ class _PrivacyState extends State<Privacy> {
         leading: IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(FontAwesome.chevron_left_solid, size: 20, color: white)),
         title: const Text("Privacy", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: white)),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Chip(
-                backgroundColor: green.withOpacity(.3),
-                deleteIconColor: white.withOpacity(.6),
-                onDeleted: () {},
-                padding: const EdgeInsets.all(16),
-                avatar: const Icon(FontAwesome.lock_solid, color: green, size: 25),
-                label: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text("Privacy checkup", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: white.withOpacity(.6))),
-                    RichText(
-                      text: const TextSpan(
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: "Control your privacy and choose the right settings for you. ",
-                            style: TextStyle(fontSize: 14, color: white, fontWeight: FontWeight.w500),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(16),
+              margin: const EdgeInsets.all(16),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: green.withOpacity(.1)),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Icon(FontAwesome.lock_solid, size: 25, color: green),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const SizedBox(height: 5),
+                        const Text("Privacy checkup", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: white)),
+                        GestureDetector(
+                          onTap: () {},
+                          child: RichText(
+                            text: const TextSpan(
+                              children: <TextSpan>[
+                                TextSpan(text: "Control your privacy and choose the right settings for you. ", style: TextStyle(fontSize: 14, color: white, fontWeight: FontWeight.w500)),
+                                TextSpan(text: "Start checkup", style: TextStyle(fontSize: 14, color: Colors.green, fontWeight: FontWeight.bold)),
+                              ],
+                            ),
                           ),
-                          TextSpan(
-                            text: "Start checkup",
-                            style: TextStyle(fontSize: 14, color: Colors.green, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Icon(FontAwesome.x_solid, size: 15, color: white.withOpacity(.6)),
+                  )
+                ],
               ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[],
-                ),
-              )
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[],
+              ),
+            )
+          ],
         ),
       ),
     );
