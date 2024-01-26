31e0064c64531554a88aa293ba3ab05b545e8a60
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-26 18:14:39 +0100

    ModifiedM lib/views/settings/account/request_account_info.dart

--- lib/views/settings/account/request_account_info.dart
+++ lib/views/settings/account/request_account_info.dart
@@ -20,41 +20,20 @@ class RequestAccountInfo extends StatelessWidget {
           children: <Widget>[
             Text("Account information", style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: white.withOpacity(.6))),
             const SizedBox(height: 20),
+            Divider(height: .5, thickness: .5, color: white.withOpacity(.6)),
+            const SizedBox(height: 20),
             Row(
               children: <Widget>[
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(item["title"], style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: white)),
-                      const SizedBox(height: 5),
-                      Text(item["subtitle"], style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: white.withOpacity(.6))),
-                    ],
-                  ),
-                ),
-                if (item["type"] != 1) const SizedBox(width: 10),
-                item["type"] == 3
-                    ? Text(item["state"], style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: white.withOpacity(.6)))
-                    : item["type"] == 2
-                        ? StatefulBuilder(
-                            builder: (BuildContext context, void Function(void Function()) _) {
-                              return Switch(
-                                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                                value: item["radio_state"],
-                                activeColor: green,
-                                activeTrackColor: white.withOpacity(.1),
-                                inactiveThumbColor: white.withOpacity(.6),
-                                onChanged: (bool value) => _(() => item["radio_state"] = value),
-                              );
-                            },
-                          )
-                        : const SizedBox(),
+                Icon(FontAwesome.file_code_solid, size: 15, color: white.withOpacity(.6)),
+                const SizedBox(width: 20),
+                const Text("Request account report", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: white)),
               ],
             ),
             const SizedBox(height: 20),
-            Divider(height: .5, thickness: .5, color: white.withOpacity(.6)),
+            Text(item["subtitle"], style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: white.withOpacity(.6))),
             const SizedBox(height: 20),
+            Divider(height: .5, thickness: .5, color: white.withOpacity(.6)),
+            const SizedBox(height: 30),
           ],
         ),
       ),
