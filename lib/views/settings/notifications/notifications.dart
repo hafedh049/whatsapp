5db3bf7302cf799d439d2ebfbed63733582fca87
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-27 20:00:39 +0100

    ModifiedM lib/views/settings/notifications/notifications.dart

--- lib/views/settings/notifications/notifications.dart
+++ lib/views/settings/notifications/notifications.dart
@@ -72,27 +72,44 @@ class _NotificationsState extends State<Notifications> {
           "subtitle": "Default (Snappy)",
           "callback": () {},
         },
+        <String, dynamic>{
+          "type": 1,
+          "title": "Vibrate",
+          "subtitle": "Off",
+          "callback": () {},
+        },
+        <String, dynamic>{
+          "type": 1,
+          "title": "Light",
+          "subtitle": "White",
+          "callback": () {},
+        },
+        <String, dynamic>{
+          "type": 2,
+          "title": "Use high priority notifications",
+          "subtitle": "Show previews of notifications at the top of the screen",
+          "state": true,
+          "callback": () {},
+        },
+        <String, dynamic>{
+          "type": 2,
+          "title": "Reaction Notifications",
+          "subtitle": "Show notifications for reactions to messages you send",
+          "state": true,
+          "callback": () {},
+        },
       ],
       "Others": <Map<String, dynamic>>[
         <String, dynamic>{
           "type": 1,
-          "title": "Chat backup",
-          "subtitle": "",
-          "icon": Bootstrap.cloud_arrow_up_fill,
+          "title": "Ringtone",
+          "subtitle": "Default (Starry Night)",
           "callback": () {},
         },
         <String, dynamic>{
           "type": 1,
-          "title": "Transfer chats",
-          "subtitle": "",
-          "icon": FontAwesome.door_open_solid,
-          "callback": () {},
-        },
-        <String, dynamic>{
-          "type": 1,
-          "title": "Chat history",
-          "subtitle": "",
-          "icon": FontAwesome.clock_rotate_left_solid,
+          "title": "Vibrate",
+          "subtitle": "Off",
           "callback": () {},
         },
       ],
@@ -106,6 +123,56 @@ class _NotificationsState extends State<Notifications> {
       appBar: AppBar(
         leading: IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(FontAwesome.chevron_left_solid, size: 20, color: white)),
         title: const Text("Notifications", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: white)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            for (final MapEntry<String, List<Map<String, dynamic>>> group in _notificationGroups.entries) ...<Widget>[
+              Text(group.key, style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: white.withOpacity(.6))),
+              const SizedBox(height: 20),
+              for (Map<String, dynamic> item in group.value) ...<Widget>[
+                Row(
+                  children: <Widget>[
+                    SizedBox(width: 40, child: item["icon"] != null ? Icon(item["icon"], size: 20, color: white.withOpacity(.6)) : null),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(item["title"], style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: item["type"] == 3 ? white.withOpacity(.3) : white)),
+                          if (item["subtitle"].isNotEmpty) ...<Widget>[
+                            const SizedBox(height: 5),
+                            Text(item["subtitle"], style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: item["type"] == 3 ? white.withOpacity(.3) : white.withOpacity(.6))),
+                          ],
+                        ],
+                      ),
+                    ),
+                    if (item["type"] != 1) const SizedBox(width: 10),
+                    item["type"] == 2
+                        ? StatefulBuilder(
+                            builder: (BuildContext context, void Function(void Function()) _) {
+                              return Switch(
+                                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                value: item["state"],
+                                activeColor: green,
+                                activeTrackColor: white.withOpacity(.1),
+                                inactiveThumbColor: white.withOpacity(.6),
+                                onChanged: (bool value) => _(() => item["state"] = value),
+                              );
+                            },
+                          )
+                        : const SizedBox(),
+                  ],
+                ),
+                const SizedBox(height: 20),
+              ],
+              Divider(height: .5, thickness: .5, color: white.withOpacity(.6)),
+              const SizedBox(height: 20),
+            ],
+          ],
+        ),
       ),
     );
   }
