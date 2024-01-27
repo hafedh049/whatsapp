07c05e4c17a4801e856665592916393be5066831
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-27 19:00:37 +0100

    Modified  lib/views/settings/avatar/avatar.dart ?? assets/images/avatar.png

--- lib/views/settings/avatar/avatar.dart
+++ lib/views/settings/avatar/avatar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+import '../../../utils/shared.dart';
 
 class Avatar extends StatelessWidget {
   const Avatar({super.key});
@@ -8,7 +11,18 @@ class Avatar extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(FontAwesome.chevron_left_solid, size: 20, color: white)),
-        title: const Text("Delete this account", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: white)),
+        title: const Text("Avatar", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: white)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[],
+          ),
+        ),
       ),
     );
   }
