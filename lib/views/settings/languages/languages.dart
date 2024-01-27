1933961f9d83cba868afe6a4573fa22ace5c081f
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-27 20:50:40 +0100

    ModifiedM lib/views/settings/languages/languages.dart

--- lib/views/settings/languages/languages.dart
+++ lib/views/settings/languages/languages.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:whatsapp/utils/shared.dart';
 
@@ -10,10 +13,11 @@ class Languages extends StatefulWidget {
 }
 
 class _LanguagesState extends State<Languages> {
+  String _language = "English";
+  Future<List<Map<String,String>>> _load()async =>json.decode(await rootBundle.loadString("assets/jsons/languages.json")).cast<Map<String,String>>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(24),
+    return SizedBox(
       height: MediaQuery.sizeOf(context).height * .5,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -21,11 +25,39 @@ class _LanguagesState extends State<Languages> {
         children: <Widget>[
           Row(
             children: <Widget>[
-              IconButton(
-                onPressed: () {},
-                icon: Icon(FontAwesome.x_solid, size: 15, color: white.withOpacity(.6)),
+              SizedBox(
+                width: 40,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(FontAwesome.x_solid, size: 15, color: white.withOpacity(.6)),
+                ),
               ),
+              const Text("App language", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: white)),
             ],
+          ),
+          const SizedBox(height: 20),
+          Divider(thickness: .3, height: .3, color: white.withOpacity(.6)),
+          const SizedBox(height: 20),
+          FutureBuilder<List<Map<String,String>>>(
+            future: _load(),
+            builder: (BuildContext context,AsyncSnapshot<List<Map<String,String>>> snapshot) {
+             if(snapshot.hasData){
+               return StatefulBuilder(
+
+                builder: (BuildContext context, void Function(void Function()) _) {
+                  return ListView.separated(
+                    itemBuilder: (BuildContext context, int index) => Row(
+                      children: <Widget>[
+                        SizedBox(width: 40, child: Radio<String>(value: value, groupValue: _language, onChanged: (String? value) => ,),),
+                      ],
+                    ),
+                    separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 20),
+                    itemCount: 30,
+                  );
+                }
+              );
+             }
+            }
           ),
         ],
       ),
